e72e0a2284af4b9465fd56d1471fb4cf997f302c
EmilienCompteEtude <emilien.cousin@uphf.fr>
2026-01-05 18:16:47 +0100

    Add files via upload

--- Serveur.java
+++ Serveur.java
@@ -1,3 +1,5 @@
+package dwEnchere;
+
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
@@ -11,20 +13,21 @@ import java.util.jar.Attributes.Name;
 
 public class Serveur {
 	
-	ArrayList<String> name =new ArrayList<>();
+	ArrayList<String> name =new ArrayList<>();		// Dans le futur, check si probleme différent nom
+	
 	public ArrayList<String> getName() {
 		return name;
 	}
 
-
-	private static int prix;
-	private static String currentWinner;
-	private static Date date;
+	static String nom = "NONE";
+	static int prix;
+	static ServiceStoC currentWinner;
+	static Date date = new Date(System.currentTimeMillis()+30*1000);
 
 	public static void main(String[] args) {
 		try {
 			prix = 1000;
-			currentWinner = "";
+			currentWinner = null;
 			
 			LocateRegistry.createRegistry(1099);
 
@@ -33,6 +36,34 @@ public class Serveur {
 			
 			System.out.println("Service d'Enchere créé ...");
 
+			
+			
+			Thread t = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						Date d = new Date();
+						Thread.sleep(30*1000);
+						
+							try {
+								currentWinner.afficheGagnant();
+								
+							} catch (RemoteException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						
+						
+						
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+			t.start();
+			
 		} catch (MalformedURLException | RemoteException | AlreadyBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- ServiceCtoS.java
+++ ServiceCtoS.java
@@ -1,9 +1,11 @@
+package dwEnchere;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ServiceCtoS extends Remote{
 
 	public void haveJoined(ServiceStoC sn, String name) throws RemoteException;
-	public void proposePrix(ServiceStoC sn, int prix) throws RemoteException;
+	public void proposePrix(ServiceStoC sn, int prix, String nom) throws RemoteException;
 	
 }
